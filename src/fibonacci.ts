3fe8da5c2a3f085d8dadf9fcb96469ac4bc553cf
Ali Taheri Motlagh <alitaherimotlagh@yahoo.com>
2024-05-27 12:53:31 +0330

    before meeting 🏩

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -2,7 +2,17 @@ function fib(n: number) {
   const fiboNumbers = [0, 1];
   for (let index = 1; index < n; index++) {
     fiboNumbers[index + 1] = fiboNumbers[index] + fiboNumbers[index - 1];
+    console.log(fiboNumbers[index + 1]);
   }
   return fiboNumbers.length;
 }
 console.log(fib(7));
+
+function fibonacci(num) {
+  if (num == 1) return 0; 
+  if (num == 2) return 1;
+  console.log(num);
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+console.log(fibonacci(7));
